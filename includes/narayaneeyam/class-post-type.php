c1416861b5d546bae9a8ffa9bf7296c7e3b0f95d
Varun <varunsridharan23@gmail.com>
2020-09-02 20:25:31 +0530

    Updated

--- includes/narayaneeyam/class-post-type.php
+++ includes/narayaneeyam/class-post-type.php
@@ -20,11 +20,11 @@ class Post_Type extends Base {
 	public function __construct() {
 		wponion_admin_columns( 'narayaneeyam_lyrics', __( 'Part 1 Details', 'aanmeegasaaral' ), array(
 			&$this,
-			'render_part1_column',
+			'render_parts_column',
 		) );
-		wponion_admin_columns( 'narayaneeyam_lyrics', __( 'Part 2 Details', 'aanmeegasaaral' ), array(
+		wponion_admin_columns( 'narayaneeyam_lyrics', __( 'Shortcodes', 'aanmeegasaaral' ), array(
 			&$this,
-			'render_part2_column',
+			'render_shortcodes_column',
 		) );
 
 		add_filter( 'user_can_richedit', array( &$this, 'disable_wysiwyg_for_CPT' ) );
@@ -56,23 +56,26 @@ class Post_Type extends Base {
 		return $default;
 	}
 
-	protected function render_part_details( $post_id, $part = 1 ) {
+	protected function render_part_details( $post_id, $part = 1, $type = 'shortcode' ) {
 		$data = wpo_post_meta( '_narayaneeyam_part' . $part, $post_id );
-		return <<<HTML
-<small>
-<span > From : {$data->get( 'from' )}</span><br/>
-<span > Till : {$data->get( 'to' )}</span><br/>
-<span > Video : {$data->get( 'video_link' )}</span><br/>
-<span > Shortcode : [narayaneeyam_lyrics id="${post_id}" from="{$data->get( 'from' )}" to="{$data->get( 'to' )}"]</span>
-</small>
+
+		if ( 'shortcode' === $type ) {
+			return <<<HTML
+<span >[narayaneeyam_lyrics id="${post_id}" from="{$data->get( 'from' )}" to="{$data->get( 'to' )}"]</span>
 HTML;
+		}
+
+		return <<<HTML
+<span> {$data->get( 'from' )} - {$data->get( 'to' )} => <a href="{$data->get( 'video_link' )}">{$data->get( 'video_link' )}</a></span>
+HTML;
+
 	}
 
-	public function render_part1_column( $post_id ) {
-		return $this->render_part_details( $post_id, 1 );
+	public function render_parts_column( $post_id ) {
+		return $this->render_part_details( $post_id, 1, 'data' ) . '<br/>' . $this->render_part_details( $post_id, 2, 'data' );
 	}
 
-	public function render_part2_column( $post_id ) {
-		return $this->render_part_details( $post_id, 2 );
+	public function render_shortcodes_column( $post_id ) {
+		return $this->render_part_details( $post_id, 1 ) . '<br/>' . $this->render_part_details( $post_id, 2 );
 	}
 }
